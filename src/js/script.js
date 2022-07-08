eb377b7ca682a89dc67eac7f1fa2cea6e082f62a
Danil Kulikov <daniel.dazert@gmail.com>
2022-07-08 19:32:01 +0200

    fifth commit: catalog has been added

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,24 @@ $(document).ready(function(){
         }
       ]
     });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+      $(this)
+        .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-card__content').eq(i).toggleClass('catalog-card__content--active');
+          $('.catalog-card__backside').eq(i).toggleClass('catalog-card__backside--active');
+        })
+      });
+    };
+
+    toggleSlide('.catalog-card__link');
+    toggleSlide('.catalog-card__link-back');
   });
+
