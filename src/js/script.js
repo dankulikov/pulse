bbf1de642379707781867754e4336edf39eff2d2
Danil Kulikov <daniel.dazert@gmail.com>
2022-07-10 12:31:50 +0200

    seventh commit: modal windows has been added

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,52 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
-      speed: 1000,
-      // adaptiveHeight: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.svg"></button>',
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            dots: true,
-            arrows: false
-          }
-        }
-      ]
-    });
-    
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
-      $(this)
-        .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
-        .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
-    });
+		$('.carousel__inner').slick({
+			speed: 1000,
+			// adaptiveHeight: true,
+			prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.svg"></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.svg"></button>',
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						dots: true,
+						arrows: false
+					}
+				}
+			]
+		});
+		
+		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function() {
+			$(this)
+				.addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+				.closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+		});
 
-    function toggleSlide(item) {
-      $(item).each(function(i) {
-        $(this).on('click', function(e) {
-          e.preventDefault();
-          $('.catalog-card__content').eq(i).toggleClass('catalog-card__content--active');
-          $('.catalog-card__backside').eq(i).toggleClass('catalog-card__backside--active');
-        })
-      });
-    };
+		function toggleSlide(item) {
+			$(item).each(function(i) {
+				$(this).on('click', function(e) {
+					e.preventDefault();
+					$('.catalog-card__content').eq(i).toggleClass('catalog-card__content--active');
+					$('.catalog-card__backside').eq(i).toggleClass('catalog-card__backside--active');
+				})
+			});
+		};
 
-    toggleSlide('.catalog-card__link');
-    toggleSlide('.catalog-card__link-back');
-  });
+		toggleSlide('.catalog-card__link');
+		toggleSlide('.catalog-card__link-back');
+
+		// Modal
+
+		$('[data-modal=consultation]').on('click', function() {
+			$('.overlay, #consultation').fadeIn('slow');
+		});
+		$('.modal__close').on('click', function() {
+			$('.overlay, #consultation, thanks, #order').fadeOut('slow');
+		});
+		$('.button--mini').each(function(i) {
+			$(this).on('click', function() {
+				$('#order .modal__subtitle').text($('.catalog-card__subtitle').eq(i).text());
+				$('.overlay, #order').fadeIn('slow');
+			});
+		});
+	});
 
